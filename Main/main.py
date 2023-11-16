250eb3c7a8a5bf8ae4c40507c8b004302810fea6
Karen A <48069766+kaandersen@users.noreply.github.com>
2023-11-16 10:21:13 +0100

    Changed param grid to TESTING values

--- Main/main.py
+++ Main/main.py
@@ -19,6 +19,7 @@ import matplotlib.pyplot as plt
 
 from Classes import AllData, BaseModel, GCN
 from Train_Test import train, test
+from Plot_func import MultiPlotter
 from Grid_Search import grid_search
 from Bayesian_Search import bayesian_search
 from Plot_func import HyperParamSearchPlot
@@ -53,12 +54,12 @@ dataset = dataset.shuffle()
 #parameter grid - set of hyper parameters and values for grid_search to iterate over
 param_grid = {
     'dropout_rate': [0.25, 0.50, 0.75],
-    'hidden_channels': [5, 32, 64, 128],
-    'learning_rate': [0.001, 0.01, 0.1],
+    'hidden_channels': [32],
+    'learning_rate': [0.01],
     'batch_size' : [16, 32, 64, 150],
     'epochs' : [10, 50, 100, 200],
     'amount_of_layers' : [1, 2, 3, 9],
-    'optimizer' : ['SGD', 'adam', 'RMSprop'],        #String key   'SGD', 'adam', 'RMSprop'
+    'optimizer' : ['SGD', 'adam', 'rmsprop'],        #String key   'SGD', 'adam', 'RMSprop'
     'activation_function' : ['relu', 'sigmoid', 'tanh'], #'Relu', 'sigmoid', 'tanh'
     'pooling_algorithm' : ['mean', 'sum']  #'mean', 'sum', 'max'
 }
