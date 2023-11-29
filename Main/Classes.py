13a13b6bde46d75f977de6d3ab07d38af82995ab
Daniel Hilo Hansen <55060805+KYNIKER@users.noreply.github.com>
2023-11-29 08:56:11 +0100

    Added CSVWriter seed

--- Main/Classes.py
+++ Main/Classes.py
@@ -212,19 +212,27 @@ class AllData():
         self.test_probability_estimates.extend(data.test_probability_estimates)
 
 class CSVWriter():
-    def __init__(self, name):
+    def __init__(self, name, seed):
         # Create path if non-existent
         cwd = os.getcwd()
         path = os.path.join(cwd, 'results')
         os.makedirs(path, exist_ok=True)
 
         self.name = path + "/" + name + ".csv"
-        
-        with open(self.name, 'w') as csvfile:
-            fieldnames = ['dropout_rate', 'hidden_channels', 'learning_rate', 'batch_size', 'epochs', 'amount_of_layers', 'optimizer', 'activation_function', 'pooling_algorithm', 'acc', 'f1', 'roc', 'pr', 'time', 'seed'] #
-            writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
-            writer.writeheader()
-            csvfile.close()
+        if(seed):
+            with open(self.name, 'w') as csvfile:
+                fieldnames = ['dropout_rate', 'hidden_channels', 'learning_rate', 'batch_size', 'epochs', 'amount_of_layers', 'optimizer', 'activation_function', 'pooling_algorithm', 'acc', 'f1', 'roc', 'pr', 'time', 'seed'] #
+                writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+                writer.writeheader()
+                csvfile.close()
+        else:
+            with open(self.name, 'w') as csvfile:
+                fieldnames = ['dropout_rate', 'hidden_channels', 'learning_rate', 'batch_size', 'epochs', 'amount_of_layers', 'optimizer', 'activation_function', 'pooling_algorithm', 'acc', 'f1', 'roc', 'pr', 'time'] #
+                writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+                writer.writeheader()
+                csvfile.close()
+
+    
 
     def CSVWriteRow(self, params, eval : EvaluationMetricsData, time):
         self.writer.writerow({'dropout_rate': params['dropout_rate'], 'hidden_channels': params['hidden_channels'], 'learning_rate': params['learning_rate'], 'batch_size': params['batch_size'], 'epochs': params['epochs'], 'amount_of_layers': params['amount_of_layers'], 'optimizer': params['optimizer'], 'activation_function': params['activation_function'], 'pooling_algorithm': params['pooling_algorithm'], 'acc': eval.accuracy, 'f1': eval.f1, 'roc': eval.roc, 'pr': eval.pr, 'time' : time})
@@ -232,9 +240,12 @@ class CSVWriter():
     def CSVWriteRowSeed(self, row, Seed):
         self.writer.writerow({'dropout_rate': row[0], 'hidden_channels': row[1], 'learning_rate': row[2], 'batch_size': row[3], 'epochs': row[4], 'amount_of_layers': row[5], 'optimizer': row[6], 'activation_function': row[7], 'pooling_algorithm': row[8], 'acc': row[9], 'f1': row[10], 'roc': row[11], 'pr': row[12], 'time' : row[13], 'seed': Seed})
 
-    def CSVOpen(self) : 
+    def CSVOpen(self, seed) : 
         self.csvfile = open(self.name, 'a', newline = '')
-        fieldnames = ['dropout_rate', 'hidden_channels', 'learning_rate', 'batch_size', 'epochs', 'amount_of_layers', 'optimizer', 'activation_function', 'pooling_algorithm', 'acc', 'f1', 'roc', 'pr', 'time', 'seed'] #
+        if(seed):
+            fieldnames = ['dropout_rate', 'hidden_channels', 'learning_rate', 'batch_size', 'epochs', 'amount_of_layers', 'optimizer', 'activation_function', 'pooling_algorithm', 'acc', 'f1', 'roc', 'pr', 'time', 'seed'] #
+        else:
+            fieldnames = ['dropout_rate', 'hidden_channels', 'learning_rate', 'batch_size', 'epochs', 'amount_of_layers', 'optimizer', 'activation_function', 'pooling_algorithm', 'acc', 'f1', 'roc', 'pr', 'time'] #
         self.writer = csv.DictWriter(self.csvfile, fieldnames = fieldnames)
 
     def CSVClose(self):
