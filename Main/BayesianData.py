55ba441c2b23e337ef17a7a85b5a59db40777e08
Daniel Hilo Hansen <55060805+KYNIKER@users.noreply.github.com>
2023-11-29 15:22:04 +0100

    NewBayesCombined

--- Main/BayesianData.py
+++ Main/BayesianData.py
@@ -1,8 +1,8 @@
 from Classes import CSVWriter 
 import csv
 
-BayesCombined = CSVWriter('BayesCombined')
-BayesCombined.CSVOpen()
+BayesCombined = CSVWriter('BayesCombined', Seed=True)
+BayesCombined.CSVOpen(Seed=True)
 
 #with open('./results/Bayes0.csv', mode='r') as csv_file:
 #    csv
@@ -13,11 +13,20 @@ BayesCombined.CSVOpen()
             print(row)"""
 
 
-for i in range(100):
+for i in range(55):
     with open('./results/Bayes/Bayes' + str(i) + '.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
             if(line_count > 1):
                 BayesCombined.CSVWriteRowSeed(row, 100 + i)
+            line_count += 1
+
+for i in range(45):
+    with open('./results/Bayes/Bayes' + str(i + 55) + '.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if(line_count > 0):
+                BayesCombined.CSVWriteRowSeed(row, 100 + i + 55)
             line_count += 1
