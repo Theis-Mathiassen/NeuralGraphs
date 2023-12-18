5abbfec5ac47a391c5ee6b5873a05fd3382b5c55
andreasholt <aholt21@student.aau.dk>
2023-12-18 17:34:01 +0100

    @fix: change distribution plots

--- Main/PlotGridSearch.py
+++ Main/PlotGridSearch.py
@@ -23,45 +23,8 @@ def filter_data_from_csv(hyperparameter_category, hyperparameters_within_categor
 
     return rocs
 
-#print(sgd_ROC[0:4])
-#print(adam_ROC[0:4])
-#print(rmsprop_ROC[0:4])
-
-# print(df.roc[0]) #0.7000000000001
-# print(type(df.roc[0])) # numpy.float64
-
-# selects all rows with roc == 0.0
-test_for_null = all_data.loc[all_data['roc'] == 0.0]
-
-# creates csv with all rows that resulted in an ROC value = 0.0
-test_for_null.to_csv('Main/test_for_null.csv', index=False)
 
 
-
-
-
-
-
-
-
-def create_ranges(data, num_ranges=100):
-    # Find the min and max
-    min_val = np.min(data)
-    max_val = np.max(data)
-
-    # Create 100 ranges 
-    value_ranges = np.linspace(min_val, max_val, num_ranges + 1)
-
-    # #data points
-    counts, _ = np.histogram(data, bins=value_ranges)
-
-    return counts
-
-your_dataset = np.random.rand(1000) * 100  
-
-counts_in_ranges = create_ranges(your_dataset)
-
-print("Number of points in each range:", counts_in_ranges)
 
 
 def create_ranges(data, num_ranges=100):
@@ -98,12 +61,12 @@ def plot_auc_count(datasets, labels, title):
 
 
     plt.title('{}.'.format(title))
-    plt.xlabel('AUROC')
+    plt.xlabel('AUC-ROC')
     plt.ylabel('Number of configurations')
 
     tick_positions = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     plt.xticks(tick_positions)
-    plt.grid()
+    #plt.grid()
 
     #plt.show()
 
@@ -119,28 +82,28 @@ hyperparameters = ["SGD", "adam", "rmsprop"]
 datasets= filter_data_from_csv("optimizer", hyperparameters)
 print(datasets)
 labels = ["SGD", "ADAM", "RMSProp"]
-plot_auc_count(datasets, labels, "Distribution of AUROC Scores for Optimizers")
+plot_auc_count(datasets, labels, "Distribution of AUC-ROC Scores for Optimizers")
 
 # Plot for # of gcn layers
 hyperparameters = [1, 2, 3, 9]
 datasets= filter_data_from_csv("amount_of_layers", hyperparameters)
 print(datasets)
 labels = ["1", "2", "3", "9"]
-plot_auc_count(datasets, labels, "Distribution of AUROC Scores for GCN Layers")
+plot_auc_count(datasets, labels, "Distribution of AUC-ROC Scores for GCN Layers")
 
 # Plot for pooling algorithms
 hyperparameters = ["mean", "sum"]
 datasets= filter_data_from_csv("pooling_algorithm", hyperparameters)
 print(datasets)
 labels = ["Mean", "Sum"] # Should be same order as hyperparameters.
-plot_auc_count(datasets, labels, "Distribution of AUROC Scores for Pooling Algorithms")
+plot_auc_count(datasets, labels, "Distribution of AUC-ROC Scores for Pooling Algorithms")
 
 # Plot for batch size
 hyperparameters = [16, 32, 64, 150]
 datasets= filter_data_from_csv("batch_size", hyperparameters)
 print(datasets)
 labels = ["16", "32", "64", "150"] # Should be same order as hyperparameters.
-plot_auc_count(datasets, labels, "Occurences of a given ROC score within a range for BS")
+plot_auc_count(datasets, labels, "Occurences of a given AUC-ROC score within a range for BS")
 
 
 # Plot for learning rate
@@ -148,119 +111,213 @@ hyperparameters = [0.1, 0.01, 0.001]
 datasets= filter_data_from_csv("learning_rate", hyperparameters)
 print(datasets)
 labels = ["0.1", "0.01", "0.001"] # Should be same order as hyperparameters.
-plot_auc_count(datasets, labels, "Distribution of AUROC Scores for Learning Rate")
+plot_auc_count(datasets, labels, "Distribution of AUC-ROC Scores for Learning Rate")
 
 # Plot for number of neurons
 hyperparameters = [5, 32, 64, 128]
 datasets= filter_data_from_csv("hidden_channels", hyperparameters)
 print(datasets)
 labels = ["5", "32", "64", "128"] # Should be same order as hyperparameters.
-plot_auc_count(datasets, labels, "Occurences of a given ROC score within a range for hidden channels")
+plot_auc_count(datasets, labels, "Occurences of a given AUC-ROC score within a range for hidden channels")
 
 # Plot for dropout rate
 hyperparameters = [0.25, 0.5, 0.75]
 datasets= filter_data_from_csv("dropout_rate", hyperparameters)
 print(datasets)
 labels = ["0.25", "0.5", "0.75"] # Should be same order as hyperparameters.
-plot_auc_count(datasets, labels, "Occurences of a given ROC score within a range for dropout rate")
+plot_auc_count(datasets, labels, "Occurences of a given AUC-ROC score within a range for dropout rate")
 
 # Plot for number of epochs
 hyperparameters = [10, 50, 100, 200]
 datasets= filter_data_from_csv("epochs", hyperparameters)
 print(datasets)
 labels = ["10", "50", "100", "200"] # Should be same order as hyperparameters.
-plot_auc_count(datasets, labels, "Occurences of a given ROC score within a range for epochs")
+plot_auc_count(datasets, labels, "Occurences of a given AUC-ROC score within a range for epochs")
 
 # Plot for number of activation function
 hyperparameters = ["relu", "sigmoid", "tanh"]
 datasets= filter_data_from_csv("activation_function", hyperparameters)
 print(datasets)
 labels = ["ReLU", "Sigmoid", "Tanh"] # Should be same order as hyperparameters.
-plot_auc_count(datasets, labels, "Distribution of AUROC Scores for Activation Functions")
+plot_auc_count(datasets, labels, "Distribution of AUC-ROC Scores for Activation Functions")
 
 
 
 
+##################
+
+plt.show()
 
 
-# Multiplot
 
 
-def plot_auc_count2(ax, datasets, labels, title):
-    # Plotting
+import numpy as np
+import matplotlib.pyplot as plt
+
+NUM_RANGES = 50
+
+def filter_data_from_csv(hyperparameter_category, hyperparameters_within_category):
+    rocs = []
+    for hyperparameter in hyperparameters_within_category:
+        filter = all_data[all_data[hyperparameter_category] == hyperparameter]
+        rocs.append(filter[['roc']])
+    return rocs
+
+def create_ranges(data, num_ranges=100):
+    min_val = np.min(data)
+    max_val = np.max(data)
+    value_ranges = np.linspace(min_val, max_val, num_ranges + 1)
+    counts, _ = np.histogram(data, bins=value_ranges)
+    return counts, value_ranges
+
+def plot_auc_count(datasets, labels, title, ax, num_ranges=50):
+    colors = ["black", "springgreen", "red", "blue"]
+    n = 4 - len(datasets)
+    c = colors[n:]
+    counts = np.ndarray(shape=(len(datasets), num_ranges))
+    bins = np.ndarray(shape=(len(datasets), num_ranges + 1))
+    also_bins = np.ndarray(shape=(len(datasets), num_ranges))
+    X = np.linspace(0, 1, num_ranges)
+
     for i, dataset in enumerate(datasets):
-        counts_in_ranges, value_ranges = create_ranges(dataset)
-        ax.plot(value_ranges[:-1], counts_in_ranges, linestyle='-', label="{}".format(labels[i]))
-        ax.legend(loc='upper left')
+        counts[i], bins[i] = np.histogram(dataset, bins=num_ranges)
 
+    for i in range(0, len(bins)):
+        for j in range(0, len(bins[i]) - 1):
+            also_bins[i][j] = bins[i][j]
+
+    for i in range(0, len(also_bins)):
+        ax.hist(also_bins[i], X, weights=counts[i], fill=True, label=labels[i], color=c[i], alpha=0.2, edgecolor=c[i])
+
+    ax.legend(loc='upper left')
     ax.set_title('{}.'.format(title))
-    ax.set_xlabel('AUROC')
-    ax.set_ylabel('Number of models')
+    ax.set_xlabel('AUC-ROC')
+    ax.set_ylabel('Number of configurations')
 
     tick_positions = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     ax.set_xticks(tick_positions)
 
-# Create subplots
-fig, axs = plt.subplots(3, 3, figsize=(15, 15))
-plt.subplots_adjust(wspace=0.4, hspace=0.4)
-plt.suptitle("Distribution of AUROC Scores for Different Hyperparameters", fontsize=16)
+fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(15, 15), sharex=True, sharey=True)
+plt.subplots_adjust(hspace=0.8, wspace=0.8)  # Adjust the spacing between subplots
 
 
-# Plotting for optimizers
-hyperparameters = ["SGD", "adam", "RMSprop"]
-datasets = filter_data_from_csv("optimizer", hyperparameters)
-labels = ["SGD", "ADAM", "RMSProp"]
-plot_auc_count2(axs[0, 0], datasets, labels, "Optimizers")
+#fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(15, 15))
 
-# Plotting for # of gcn layers
-hyperparameters = [1, 2, 3, 9]
-datasets = filter_data_from_csv("amount_of_layers", hyperparameters)
-labels = ["1", "2", "3", "9"]
-plot_auc_count2(axs[0, 1], datasets, labels, "GCN Layers")
+# Plotting for each hyperparameter
+hyperparameters_list = [
+    ["optimizer", ["SGD", "adam", "rmsprop"]],
+    ["amount_of_layers", [1, 2, 3, 9]],
+    ["pooling_algorithm", ["mean", "sum"]],
+    ["batch_size", [16, 32, 64, 150]],
+    ["learning_rate", [0.1, 0.01, 0.001]],
+    ["hidden_channels", [5, 32, 64, 128]],
+    ["dropout_rate", [0.25, 0.5, 0.75]],
+    ["epochs", [10, 50, 100, 200]],
+    ["activation_function", ["relu", "sigmoid", "tanh"]]
+]
 
-# Plotting for pooling algorithms
-hyperparameters = ["mean", "sum"]
-datasets = filter_data_from_csv("pooling_algorithm", hyperparameters)
-labels = ["Mean", "Sum"]
-plot_auc_count2(axs[0, 2], datasets, labels, "Pooling Algorithms")
+titles = [
+    "Optimizers",
+    "GCN Layers",
+    "Pooling Algorithms",
+    "Batch Size",
+    "Learning Rate",
+    "Hidden Channels",
+    "Dropout Rate",
+    "Epochs",
+    "Activation Functions"
+]
 
-# Plotting for batch size
-hyperparameters = [16, 32, 64, 150]
-datasets = filter_data_from_csv("batch_size", hyperparameters)
-labels = ["16", "32", "64", "150"]
-plot_auc_count2(axs[1, 0], datasets, labels, "Batch Size")
+for i, (hyperparameter_category, hyperparameters) in enumerate(hyperparameters_list):
+    row = i // 3
+    col = i % 3
+    datasets = filter_data_from_csv(hyperparameter_category, hyperparameters)
+    plot_auc_count(datasets, hyperparameters, titles[i], axes[row, col], num_ranges=NUM_RANGES)
 
-# Plotting for learning rate
-hyperparameters = [0.1, 0.01, 0.001]
-datasets = filter_data_from_csv("learning_rate", hyperparameters)
-labels = ["0.1", "0.01", "0.001"]
-plot_auc_count2(axs[1, 1], datasets, labels, "Learning Rate")
+plt.suptitle("Distribution of AUC-ROC Scores for Different Hyperparameters", fontsize=16)
+plt.tight_layout(rect=[0, 0, 1, 0.9])  # Add a margin below the title
 
-# Plotting for number of neurons
-hyperparameters = [5, 32, 64, 128]
-datasets = filter_data_from_csv("hidden_channels", hyperparameters)
-labels = ["5", "32", "64", "128"]
-plot_auc_count2(axs[1, 2], datasets, labels, "Hidden Channels")
+# Adjust layout to prevent clipping of titles
 
-# Plotting for dropout rate
-hyperparameters = [0.25, 0.5, 0.75]
-datasets = filter_data_from_csv("dropout_rate", hyperparameters)
-labels = ["0.25", "0.5", "0.75"]
-plot_auc_count2(axs[2, 0], datasets, labels, "Dropout Rate")
-
-# Plotting for number of epochs
-hyperparameters = [10, 50, 100, 200]
-datasets = filter_data_from_csv("epochs", hyperparameters)
-labels = ["10", "50", "100", "200"]
-plot_auc_count2(axs[2, 1], datasets, labels, "Epochs")
-
-# Plotting for number of activation function
-hyperparameters = ["relu", "sigmoid", "tanh"]
-datasets = filter_data_from_csv("activation_function", hyperparameters)
-labels = ["ReLU", "Sigmoid", "Tanh"]
-plot_auc_count2(axs[2, 2], datasets, labels, "Activation Functions")
+# Show the combined plot
+plt.show()
 
 
-# Adjust layout
-#plt.tight_layout()
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+NUM_RANGES = 50
+
+def filter_data_from_csv(hyperparameter_category, hyperparameters_within_category):
+    rocs = []
+    for hyperparameter in hyperparameters_within_category:
+        filter = all_data[all_data[hyperparameter_category] == hyperparameter]
+        rocs.append(filter[['roc']])
+    return rocs
+
+def create_ranges(data, num_ranges=100):
+    min_val = np.min(data)
+    max_val = np.max(data)
+    value_ranges = np.linspace(min_val, max_val, num_ranges + 1)
+    counts, _ = np.histogram(data, bins=value_ranges)
+    return counts, value_ranges
+
+def plot_auc_count(datasets, labels, title, ax, num_ranges=50):
+    colors = ["black", "springgreen", "red", "blue"]
+    n = 4 - len(datasets)
+    c = colors[n:]
+    bins = np.linspace(0, 1, num_ranges + 1)  # Use the same bin edges for all datasets
+    X = np.linspace(0, 1, num_ranges)
+
+    for i, dataset in enumerate(datasets):
+        ax.hist(dataset, bins=bins, density=True, alpha=0.5, label=labels[i], color=c[i])
+
+    ax.legend(loc='upper left')
+    ax.set_title('{}.'.format(title))
+    ax.set_xlabel('AUC-ROC')
+    ax.set_ylabel('Density')
+
+    tick_positions = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    ax.set_xticks(tick_positions)
+
+# Create a 3x3 grid of subplots
+fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(15, 15))
+
+# Plotting for each hyperparameter
+hyperparameters_list = [
+    ["optimizer", ["SGD", "adam", "rmsprop"]],
+    ["amount_of_layers", [1, 2, 3, 9]],
+    ["pooling_algorithm", ["mean", "sum"]],
+    ["batch_size", [16, 32, 64, 150]],
+    ["learning_rate", [0.1, 0.01, 0.001]],
+    ["hidden_channels", [5, 32, 64, 128]],
+    ["dropout_rate", [0.25, 0.5, 0.75]],
+    ["epochs", [10, 50, 100, 200]],
+    ["activation_function", ["relu", "sigmoid", "tanh"]]
+]
+
+titles = [
+    "Distribution of AUC-ROC Scores for Optimizers",
+    "Distribution of AUC-ROC Scores for GCN Layers",
+    "Distribution of AUC-ROC Scores for Pooling Algorithms",
+    "Occurences of a given AUC-ROC score within a range for BS",
+    "Distribution of AUC-ROC Scores for Learning Rate",
+    "Occurences of a given AUC-ROC score within a range for hidden channels",
+    "Occurences of a given AUC-ROC score within a range for dropout rate",
+    "Occurences of a given AUC-ROC score within a range for epochs",
+    "Distribution of AUC-ROC Scores for Activation Functions"
+]
+
+for i, (hyperparameter_category, hyperparameters) in enumerate(hyperparameters_list):
+    row = i // 3
+    col = i % 3
+    datasets = filter_data_from_csv(hyperparameter_category, hyperparameters)
+    plot_auc_count(datasets, hyperparameters, titles[i], axes[row, col], num_ranges=NUM_RANGES)
+
+# Adjust layout to prevent clipping of titles
+plt.tight_layout()
+
+# Show the combined plot
 plt.show()
