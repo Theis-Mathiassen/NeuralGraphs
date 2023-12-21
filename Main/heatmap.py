1da8375efddd5289d3b965446b4cf1b14c4516a8
AndreasHolt <aholt21@student.aau.dk>
2023-12-21 18:23:18 +0100

    refactor: runtime options + clean/comment code

--- Main/heatmap.py
+++ Main/heatmap.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 
 
-mpl.rcParams['figure.dpi'] = 400
+mpl.rcParams["figure.dpi"] = 400
+
 
 # def GetHeatData() :
 #     usecols = ["dropout_rate", "hidden_channels", "learning_rate", "batch_size","epochs","amount_of_layers","optimizer","activation_function","pooling_algorithm", "roc"]
@@ -20,37 +21,48 @@ mpl.rcParams['figure.dpi'] = 400
 #
 #
 def heatMap():
-    #data = GetHeatData() # Gets data in the format that a clustermap desires
-
+    # data = GetHeatData() # Gets data in the format that a clustermap desires
 
     # Assuming 'results' is a DataFrame with your data
     # Sample the data to avoid cluttering the heatmap
-    #sample_size = min(1000, len(data))
-
+    # sample_size = min(1000, len(data))
 
     # Specify the columns you want to include in the heatmap
-    columns_to_include = ['dropout_rate', 'hidden_channels', 'learning_rate', 'batch_size', 'epochs',
-                        'amount_of_layers', 'optimizer', 'activation_function', 'pooling_algorithm',
-                        'roc']
-    csv_data = pd.read_csv("../results/Grid_LastLastFr.csv")
+    columns_to_include = [
+        "dropout_rate",
+        "hidden_channels",
+        "learning_rate",
+        "batch_size",
+        "epochs",
+        "amount_of_layers",
+        "optimizer",
+        "activation_function",
+        "pooling_algorithm",
+        "roc",
+    ]
+    csv_data = pd.read_csv("results/Grid_LastLastFr.csv")
 
-    #subsampled_results = csv_data.sample(n=1000, replace=False)
+    # subsampled_results = csv_data.sample(n=1000, replace=False)
 
     # Create the heatmap
-    heatmap_data = csv_data[columns_to_include].pivot_table(aggfunc="mean", index=['optimizer', 'learning_rate'], columns=['pooling_algorithm', 'activation_function', 'amount_of_layers'], values='roc')
+    heatmap_data = csv_data[columns_to_include].pivot_table(
+        aggfunc="mean",
+        index=["optimizer", "learning_rate"],
+        columns=["pooling_algorithm", "activation_function", "amount_of_layers"],
+        values="roc",
+    )
     print(heatmap_data)
     seaborn.set(font_scale=0.35)
-    ax = seaborn.heatmap(heatmap_data, annot=True, cmap='viridis')
+    ax = seaborn.heatmap(heatmap_data, annot=True, cmap="viridis")
 
-# Rotate the x-axis tick labels
-    ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
+    # Rotate the x-axis tick labels
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
 
-# Rotate and align the y-axis tick labels
-    ax.set_yticklabels(ax.get_yticklabels(), rotation=0, va='center')
-    
+    # Rotate and align the y-axis tick labels
+    ax.set_yticklabels(ax.get_yticklabels(), rotation=0, va="center")
 
-    plt.title('Heatmap: AUC-ROC')
-    #plt.savefig(f'./results/heatmap.png', format='png', dpi=400)
+    plt.title("Heatmap: AUC-ROC")
+    # plt.savefig(f'./results/heatmap.png', format='png', dpi=400)
 
     plt.show()
 
